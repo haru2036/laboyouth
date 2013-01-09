8f1140cb555a53f9f7414d629429a079c2f6a182
haru2036 <hogehoge111@gmail.com>
2013-01-09 17:31:04 +0900

    ChaSenParserを書いている途中

---
+++ ChaSenParser.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*
+
+def ChaSenParser(ChaSenInput):
+	lines=ChaSenInput.splitlines(false)
+	parsedlines=[]
+	for line in lines:
+		parsedlines.add(line.split(" "))
+	parsedwords=[]
+	for word in words:
+		parsedwords.add(word[0],word[3])
+	return parsedwords
+def hinshiStrip(words)
+	for word in words:
+		if word[1] not in '一般':
+			if word[1] not in '自立':
+				wordk
+	words2=[x for x in words if x[1] not in '一般' or '自立' ]
