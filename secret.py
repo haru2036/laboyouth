8b1b6eaf9849b60e29b0ad1ebb44ab676943a626
haru2036 <hogehoge111@gmail.com>
2013-02-05 16:13:41 +0900

    わかりやすいようにSecretのところに適当な値を書いておいた

--- secret.py
+++ secret.py
@@ -1,6 +1,6 @@
 dict = {
-'consumer_key':'uatEQejyYrZ1zrm09VxKQ',
-'consumer_secret':'4cvG4xoxE8g1srCm85fIqaoPGb1GF5UFDdFZ20r8Hg',
-'access_token_key':'784547701-PgGqZ1QS3YDKStDbNjsPXOeGq9Tvsg8hO7o0MFAX',
-'access_token_secret':'yZo3M4XKy925BpnvfQkEydYOvUC1B5vjvZh6ClMU0'
+'consumer_key':'hogehoge',
+'consumer_secret':'piyopiyo',
+'access_token_key':'hogepiyo',
+'access_token_secret':'piyohoge'
 }
