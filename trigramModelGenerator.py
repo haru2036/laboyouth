38ebd86cedc17f57c562fe60bbedf0924d0564c0
haru2036 <hogehoge111@gmail.com>
2013-03-15 08:10:21 +0900

    軽量化中。ただしまだ動かない

--- trigramModelGenerator.py
+++ trigramModelGenerator.py
@@ -24,25 +24,37 @@ def generateModel(sentence):
 	return freq1
 def generateModel_SpaceSaving(args):
 	sentence,k=args
-	print sentence
 	itemlist2=mecabCaller.parse(sentence)
 	print "job.start()"
 	print "generating..."
 	cj={}
+	x=itemlist2[0]
+	y=itemlist2[1]
+	z=itemlist2[2]
+	minimum=[]
+	minimumvalue=0
 	for counter in xrange(len(itemlist2)-2):
-		x=itemlist2[counter]
-		y=itemlist2[counter+1]
 		z=itemlist2[counter+2]
 		i=x,y,z
 		if i in cj:
 			cj[i]+=1
+			minimum.remove(i)
+			if len(minimum) is 0:
+				minimum.append(i)
+				minimumvalue+=1
+				#TODO:ここにcjのvalueがminimumvalueと等しい物だけを取り出してminimumに追加する処理を書く。
 		elif len(cj)<k:
 			cj[i]=1
+			minimum.append(i)
+			minimumvalue=1
 		else:
-			#とりあえず真似してみる
-			j=min(cj,key=lambda x:cj[x])
+			j=minimum.pop()
 			cj[i]=cj[j]+1
 			del(cj[j])
+			if len(minimum) is 0:
+				#TODO:ここにその時点でのcjで最小値を持つ項目のkeyをすべてminimumにappendして、そのValue（すべて同じはず）をminimumvalueに代入する処理を書く。
+		x=y
+		y=z
 	print "end of generate"
 	return cj
 def cjtofreq(cj):
