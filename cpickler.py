8c8ed3a66cdabb8f490b28263943c2fe4a5d40ef
haru2036 <hogehoge111@gmail.com>
2013-04-02 03:31:50 +0900

    release1.0

--- cpickler.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*
-import cPickle
-def topickle(importdata,filename="pickle.sampleapitweets.dump"):
-	with open(filename,"wb") as f:
-		cPickle.dump(importdata,f)
-def frompickle(filename):
-	with open(filename,"rb") as f:
-		return cPickle.load(f)
